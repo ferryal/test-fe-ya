b10866fcde260c09e2c7e2c566b9e6fec50f5443
Ferryal Fajar <ferryal21@gmail.com>
2024-05-29 13:05:37 +0700

    [feat] add page home, login & register

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import AuthLoginView from '@/layouts/auth/login-view';
+
+export const metadata: Metadata = {
+  title: 'YouApp - Login',
+  icons: [
+    {
+      rel: 'icon',
+      url: '/assets/image/android-icon-144x144.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/assets/image/android-icon-144x144.png',
+    },
+  ],
+};
+
+export default function HomePage() {
+  return <AuthLoginView />;
+}
