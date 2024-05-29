919b09eb44f8fda66950e8e4b37946747b8b8632
Ferryal Fajar <ferryal21@gmail.com>
2024-05-29 10:44:47 +0700

    [fix] adjust rewrite env url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     serverActions: true,
+  },
+  env: {
+    NEXT_BASE_API_URL: process.env.NEXT_BASE_API_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_BASE_API_URL}/:path*`,
+        basePath: false
+      },
+    ];
   },
 };
 
