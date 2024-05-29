fa9a7680f4c022d1fd9d70038a30aa3953a7def0
Ferryal Fajar <ferryal21@gmail.com>
2024-05-29 11:40:15 +0700

    [feat] add utils to calculate & cookies

---
+++ src/utils/cookies.tsx
@@ -0,0 +1,14 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+export async function getCookie(name: string) {
+  const cookie = cookies();
+  const res = cookie.get(name);
+
+  return res;
+}
+
+export async function deleteCookie(name: string) {
+  cookies().delete(name);
+}
